46c4339a423c56360eda129312287ac78c24414f
Marcin <Majsyn@gmail.com>
2018-07-03 19:30:45 +0200

    4. commit

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -2,5 +2,7 @@ public class Zadanie1 {
     public static void main(String[] args) {
         System.out.println("Lubię");
         System.out.println("placki");
+        System.out.println('z');
+        System.out.println("dżemem");
     }
 }
